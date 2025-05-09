17fe22ec22c7eed931274086766df6d3a013d756
Ibrokhimova <gulasalibrokhimova0321@gmail.com>
2025-05-09 10:42:43 +0300

    Add files via upload

---
+++ lesson-1/homework1/third.py
@@ -0,0 +1,12 @@
+# Data type 
+print(type(1))
+print(type(1.5))
+print(type("Hello"))
+print(type(True))
+print(type(False))
+print(type(2+3j))
+print("Apple" *5)
+a = 20
+b = 6
+print (2**0.5)
+print(divmod(a,b))
